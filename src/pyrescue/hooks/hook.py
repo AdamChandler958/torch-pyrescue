3bff085f42ab917f8051604bd2d5ed5c4643df8e
Adam Chandler <adam.chandler@mymail.unisa.edu.au>
2025-09-24 10:21:58 +0930

    Feat: Adding setter method to notify observer

--- src/pyrescue/hooks/hook.py
+++ src/pyrescue/hooks/hook.py
@@ -22,3 +22,18 @@ class Hook(FlagManager):
     def __init__(self, name: str, is_forward_hook: bool):
         super().__init__()
         self.name = name
+        self.is_forward_hook = is_forward_hook
+        self._flag_status = False
+
+    @property
+    def flag_status(self):
+        return self._flag_status
+
+    @flag_status.setter
+    def flag_status(self, value: bool):
+        if self._flag_status != value:
+            self._flag_status = value
+            self.notify_observers(self.name, self._flag_status)
+
+    def hook(self, *args, **kwargs):
+        raise NotImplementedError("Subclasses must implement the hook method.")
